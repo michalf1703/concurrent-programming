d4bd8df24cd41884a005d296cf2a8e3596345932
mferdzyn <242383@edu.p.lodz.pl>
2023-03-16 22:03:33 +0100

    introduction zad0 - 2 tests

---
+++ TPW/TPW/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace program;
+
+public class Calculator
+{
+    public int add(int v1, int v2)
+    {
+        return v1 + v2;
+    }
+
+    public int subtract(int v1, int v2)
+    {
+        return v1 - v2;
+    }
+    public static void Main(string[] args)
+    {
+
+    }
+
+}
