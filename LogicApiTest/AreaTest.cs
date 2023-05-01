f589bc7c2bb683c6a51563928c165ba49cc6e5fc
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-05-01 17:33:31 +0200

    update logicAPI

--- LogicApiTest/AreaTest.cs
+++ LogicApiTest/AreaTest.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using NUnit.Framework;
 using Logic;
 
@@ -10,47 +9,7 @@ namespace LogicTest
         [SetUp]
         public void Setup()
         {
-            Area area = new Area(700);        
-            area.addBalls(1);
-            Assert.AreEqual(area.balls.Count, 1);
-            Assert.NotNull(test);
-            test.createBalls(3);
-            Assert.AreEqual(test.GetBalls().Count, 3);
-        }
-
-        [Test]
-        public void CreateBallsTest()
-        {
-            Area region = new Area(800);
-            region.addBalls(3);
-
-            double pX1 = region.balls[0].x;
-            double pY1 = region.balls[0].y;
-
-            double pX2 = region.balls[1].x;
-            double pY2 = region.balls[1].y;
-
-            region.MoveBall();
-
-            Assert.AreNotEqual(region.balls[0].x, pX1);
-            Assert.AreNotEqual(region.balls[0].y, pY1);
-            Assert.AreNotEqual(region.balls[1].x, pX2);
-            Assert.AreNotEqual(region.balls[1].y, pY2);
-        }
-    }
-=======
-using NUnit.Framework;
-using Logic;
-
-namespace LogicTest
-{
-    internal class AreaTest
-    {
-        LogicAbstractApi test = LogicAbstractApi.CreateApi();
-        [SetUp]
-        public void Setup()
-        {
-            Area area = new Area(700);        
+            Area area = new Area(700);
             area.addBalls(1);
             Assert.AreEqual(area.balls.Count, 1);
             Assert.NotNull(test);
@@ -78,7 +37,5 @@ namespace LogicTest
             Assert.AreNotEqual(region.balls[1].y, pY2);
         }
 
-       
+
     }
->>>>>>> main
-}
