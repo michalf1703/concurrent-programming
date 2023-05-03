b122ce8ee2111f60ba212a5a45f549fc74d3973b
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-05-03 15:51:30 +0200

    add comments

--- PresentationViewModel/ViewModel.cs
+++ PresentationViewModel/ViewModel.cs
@@ -24,6 +24,7 @@ namespace ViewModel
 
         private bool state;
 
+        //The State property is used to enable/disable the start button in the view
         public bool State
         {
             get
@@ -37,7 +38,7 @@ namespace ViewModel
             }
         }
 
-
+        //The InputText property is bound to a text box in the view
         public string InputText
         {
             get
@@ -53,6 +54,7 @@ namespace ViewModel
 
         private string errorMessage;
 
+        //The ErrorMessage property is used to display validation errors in the view
         public string ErrorMessage
         {
             get
@@ -79,6 +81,7 @@ namespace ViewModel
             this.modelApi = baseModel;
             StartButtonClick = new UserCommand(() => StartButtonClickHandler());
             Balls = new ObservableCollection<IBall>();
+            //Subscribe to the model's IObservable<IBall> to get notified of changes
             IDisposable observer = modelApi.Subscribe(x => Balls.Add(x));
         }
 
@@ -87,6 +90,7 @@ namespace ViewModel
             modelApi.AddBallsAndStart(readFromTextBox());
         }
 
+        //Parse the user input from the text box and validate it
         public int readFromTextBox()
         {
             int number;
@@ -105,6 +109,7 @@ namespace ViewModel
             return 0;
         }
 
+        //Raise the PropertyChanged event to notify the view of changes to the properties
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
